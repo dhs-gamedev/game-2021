116d57bbd70359e8cf2630fe767f19eef197ed29
adamhutchings <adam.abahot@gmail.com>
2021-10-08 20:28:56 -0700

    Add main file

---
+++ src/main/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Main file for the 2021-22 game - for now, just does Hello World.
+ */
+
+#include <iostream>
+
+int main(int argc, char ** argv) {
+    /* October 7, 2021 - this is the very beginning of it all. */
+    std::cout << "Hello, World!" << "\n";
+    return 0;
+}
