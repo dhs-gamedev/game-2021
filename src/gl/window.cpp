f4b5054ac5217dd92497f1ed561f7486bc702d58
adamhutchings <adam.abahot@gmail.com>
2021-10-08 22:40:48 -0700

    Add new GL context creation

--- src/gl/window.cpp
+++ src/gl/window.cpp
@@ -1,16 +1,23 @@
 #include "window.hpp"
 
+#include <gl/gl.h>
+
 namespace gl {
 
 Window::Window(int width, int height) {
 
     glfwInit();
 
+    GL_INIT_CONTEXT();
+
     this->wn = glfwCreateWindow(
         width, height, "Game 2021-22", nullptr, nullptr
     );
     glfwMakeContextCurrent(this->wn);
+    gladLoadGL();
     glfwShowWindow(this->wn);
+    // Test - apocalyptic gray
+    glClearColor(0.6f, 0.6f, 0.6f, 1.0f);
 
 }
 
