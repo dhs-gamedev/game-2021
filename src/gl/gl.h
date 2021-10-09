b2ab5554b78549b0c504f939c1c6528227b406c4
adamhutchings <adam.abahot@gmail.com>
2021-10-08 22:40:36 -0700

    Add version info

---
+++ src/gl/gl.h
@@ -0,0 +1,18 @@
+/**
+ * Header-only - GL contexts and whatnot.
+ */
+
+#define GL_SILENCE_DEPRECATION // macOS sayss it's deprecated. WHY?!
+
+#include <gl/glad.h>
+
+// OpenGL 4.1
+#define V_MAJ 4
+#define V_MIN 1
+
+#define GL_INIT_CONTEXT() do { \
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, V_MAJ); \
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, V_MIN); \
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); \
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); \
+} while (0)
