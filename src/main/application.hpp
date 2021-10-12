c265cde28632750d243744df9b44f89d794b7a6f
adamhutchings <adam.abahot@gmail.com>
2021-10-11 21:28:07 -0700

    Add player member and entity rendering

--- src/main/application.hpp
+++ src/main/application.hpp
@@ -5,16 +5,20 @@
 
 #pragma once
 
+#include <entity/player.hpp>
 #include <gl/window.hpp>
 
 class Application {
 
 private:
     gl::Window wn;
+    ent::Player * player;
 
 public:
     Application();
    ~Application();
     void mainloop();
+
+friend class ent::Player;
 
 };
