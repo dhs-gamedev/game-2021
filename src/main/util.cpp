9ada34fb13753f124d85c4497ebf72c9ba46ae6e
adamhutchings <adam.abahot@gmail.com>
2021-10-14 00:51:01 -0700

    Log to file

--- src/main/util.cpp
+++ src/main/util.cpp
@@ -1,5 +1,14 @@
+#include <fstream>
 #include <iostream>
 #include <main/util.hpp>
+
+namespace {
+    const char * log_file = "log.txt";
+    // If a "log file cannot be opened" warning has already been shown
+    bool log_file_warning_shown = false;
+    // Whether debugs go to standard out
+    bool debug_to_stdout = false;
+}
 
 namespace util {
 
@@ -7,7 +16,21 @@ const char *severity_text[] = {
     "Normal: ", "Debug: ", "Error: ", "Warning: ", "Fatal: "};
 
 int log(std::string message, util::Severity severity) {
-    std::cout << util::severity_text[severity] << message << std::endl;
+    std::ofstream out_file(log_file, std::ios::app);
+    if (!out_file.is_open()) {
+        if (!log_file_warning_shown) {
+            log_file_warning_shown = true;
+            debug_to_stdout = true;
+            log(
+                "Cannot print to log file, printing all logs including debugs to standard out"
+                , util::Severity::WARNING
+            );
+        }
+    }
+    std::string msg = severity_text[severity] + message;
+    if (severity != Severity::DEBUG || debug_to_stdout)
+        std::cout << msg << std::endl;
+    out_file << msg << std::endl;
 	return 0;
 }
 
