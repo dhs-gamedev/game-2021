91ba2dff0d0a739537a4230ae24c88c3ae21b6b3
Brandon Qi <71684640+Sophon96@users.noreply.github.com>
2021-10-16 16:59:44 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,12 @@
-mkdir -p build
+set -e
+
+if [[ -e build ]]; then
+  if [[ ! -d build ]]; then
+    echo "Build exists, and is not a directory!" >&2
+  fi
+else
+  mkdir build
+fi
+
 cmake -S . -B build/
 cmake --build build/
