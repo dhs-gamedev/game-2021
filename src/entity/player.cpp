6aeb52b3c3ff5903094df5afea98733cc6ed04b8
adamhutchings <adam.abahot@gmail.com>
2021-10-19 16:08:17 -0700

    Init player facing left and render with direction

--- src/entity/player.cpp
+++ src/entity/player.cpp
@@ -9,13 +9,17 @@ namespace ent {
 Player::Player(double x, double y, Application * app)
 : Entity(x, y) {
     app->player = this;
+    this->direction_facing = EntityDirection::RIGHT;
 }
 
 void Player::render() {
     tex::render_texture(
         x, y, WIDTH, HEIGHT,
         tex::RenderBasis::MID, tex::RenderBasis::LOW,
-        tex::PLAYER_TEX.get()
+        tex::PLAYER_TEX.get(),
+        (this->direction_facing == LEFT)
+            ? tex::Mirrored::MIRRORED
+            : tex::Mirrored::NORMAL
     );
 }
 
