fe49b6d980b9db7a00680b2749a90790765859d8
adamhutchings <adam.abahot@gmail.com>
2021-10-19 14:44:03 -0700

    Add profiler header

---
+++ src/util/profiler.hpp
@@ -0,0 +1,33 @@
+/**
+ * For profiling the game's performance, and possibly other things later.
+ */
+
+#pragma once
+
+namespace prof {
+
+/**
+ * The amount of strain the game is under.
+ */
+enum StrainLevel {
+    VERY_LIGHT,
+    LIGHT,
+    NORMAL,
+    HEAVY,
+    VERY_HEAVY,
+    SEVERE,
+};
+
+class Profiler {
+
+public:
+    Profiler();
+
+    /**
+     * Get the strain level from the time taken in a frame.
+     */
+    StrainLevel calculate_strain(double seconds_in_frame);
+    
+}
+
+}
