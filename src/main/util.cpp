817620e9f368194c5691d1a4a15395b09b39adb7
adamhutchings <adam.abahot@gmail.com>
2021-10-21 00:45:49 -0700

    Add unused check function

--- src/main/util.cpp
+++ src/main/util.cpp
@@ -1,5 +1,9 @@
 #include <fstream>
 #include <iostream>
+#include <stdlib.h>
+
+#include <gl/glad.h>
+
 #include <main/util.hpp>
 
 namespace {
@@ -33,6 +37,40 @@ int log(std::string message, util::Severity severity) {
     if (severity != Severity::DEBUG || debug_to_stdout)
         std::cout << msg << std::endl;
 	return 0;
+}
+
+int check_for_gl_error(ExitValue val) {
+
+    // Generic error messages
+    static struct {
+        GLenum error;
+        const char * message;
+    } error_messages[] = {
+        {GL_INVALID_ENUM,                  "Invalid enum"},
+        {GL_INVALID_VALUE,                 "Invalid value"},
+        {GL_INVALID_OPERATION,             "Invalid operation"},
+        {GL_INVALID_FRAMEBUFFER_OPERATION, "Invalid framebuffer operation"},
+        {GL_OUT_OF_MEMORY,                 "Out of memory"},
+        {GL_STACK_UNDERFLOW,               "Stack underflow"},
+        {GL_STACK_OVERFLOW,                "Stack overflow"},
+    };
+
+    auto error_val = glGetError();
+    if (error_val == GL_NO_ERROR)
+        return 0;
+    
+    for (auto& error_pair : error_messages) {
+        if (error_pair.error == error_val) {
+            if (val == EXIT_IF_ERROR) {
+                log(error_pair.message, util::Severity::FATAL);
+                exit(-1);
+            } else {
+                log(error_pair.message, util::Severity::ERROR);
+                return (int) error_val;
+            }
+        }
+    }
+    
 }
 
 } // namespace util
--- src/main/util.hpp
+++ src/main/util.hpp
@@ -15,4 +15,13 @@ enum Severity {
 
 int log(std::string message, util::Severity severity);
 
+enum ExitValue {
+    NO_EXIT,
+    EXIT_IF_ERROR,
+};
+
+// Check for GL errors and log if any have occurred. Exit if val is set and an
+// error is found. Returns nonzero if error, zero if good.
+int check_for_gl_error(ExitValue val);
+
 } // namespace util
