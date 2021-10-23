5daf1ced8e3b052f523fd4ed20416154727ce2dd
adamhutchings <adam.abahot@gmail.com>
2021-10-23 09:55:35 -0700

    Add ratio uniform

--- src/gl/shad/basic_vert.glsl
+++ src/gl/shad/basic_vert.glsl
@@ -3,9 +3,20 @@
 layout(location=0) in vec3 inVertexCoord;
 layout(location=1) in vec2 inTexCoord;
 
+uniform float ratio; // width / height
+
 out vec2 texCoord;
 
 void main() {
+
     gl_Position = vec4(inVertexCoord, 1.0);
     texCoord = inTexCoord;
+
+    if (ratio < 1.0) {
+        gl_Position.y *= ratio;
+    }
+    if (ratio > 1.0) {
+        gl_Position.x /= ratio;
+    }
+
 }
