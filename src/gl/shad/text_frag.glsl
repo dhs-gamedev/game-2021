0fcaafb114728ff6289f1e23739361029afbd559
adamhutchings <adam.abahot@gmail.com>
2021-10-25 20:59:51 -0700

    Remove syntax error

--- src/gl/shad/text_frag.glsl
+++ src/gl/shad/text_frag.glsl
@@ -5,7 +5,6 @@ in vec2 texCoord;
 uniform sampler2D tex;
 
 void main() {
-    ~?~?~?~? // syntax error
     vec4 inter = texture(tex, texCoord);
     fragColor = vec4(inter.r, inter.r, inter.r, 1.0);
     if (fragColor.r < 0.05) discard;
