adeaa81fa71d07b0e1f1439143ac1015dd5268be
adamhutchings <adam.abahot@gmail.com>
2021-10-26 00:42:36 -0700

    Add some commenting and remove an invalid decl

--- src/gl/text.hpp
+++ src/gl/text.hpp
@@ -19,10 +19,9 @@ private:
     stbtt_fontinfo info;
 
 public:
+    // Get a font's data from a file path containing the font.
      Font(const std::string& font_path);
     ~Font();
-
-    void draw(const std::string& text, float x, float y, float scale);
 
 friend class TextLabel;
 
@@ -35,10 +34,15 @@ private:
     unsigned id;
 
 public:
+    // Initialize the text label with the given text and font.
+    // EX: TextLabel("Hello World", Font("font.ttf"));
      TextLabel(const std::string& text, const Font& font);
      TextLabel() = delete;
     ~TextLabel();
 
+    // Draw at the specified position. This should be replaced later.
+    // NOTE: x and y are LOWER LEFT!
+    // Also, the text is not scaled to fit the box. Whatever.
     void draw(float x, float y, float xs, float ys);
 
 };
